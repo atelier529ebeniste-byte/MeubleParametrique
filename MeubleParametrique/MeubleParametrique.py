620465397bfcc8a700e4716f0d567627edb0d99f
atelier529ebeniste-byte <atelier529ebeniste@gmail.com>
2026-08-30 10:41:25 +0200

    Update MeubleParametrique.py
    V1.04

--- MeubleParametrique/MeubleParametrique.py
+++ MeubleParametrique/MeubleParametrique.py
@@ -9,7 +9,7 @@ import traceback
 # Numero de version affiche dans le dialogue (sous le logo, et dans
 # le bloc Mise a jour). Format N.NN. A incrementer manuellement a
 # chaque publication sur Drive/GitHub.
-ADDIN_VERSION = '1.03'
+ADDIN_VERSION = '1.04'
 
 app = None
 ui = None
