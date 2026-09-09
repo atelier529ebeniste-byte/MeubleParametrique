88336bd2d6d8e6bafefe0eaff397b48c71c33ddb
atelier529ebeniste-byte <atelier529ebeniste@gmail.com>
2026-09-09 09:13:23 +0200

    Add files via upload

--- MeubleParametrique.py
+++ MeubleParametrique.py
@@ -11,7 +11,7 @@ import traceback
 # Numero de version affiche dans le dialogue (sous le logo, et dans
 # le bloc Mise a jour). Format N.NN. A incrementer manuellement a
 # chaque publication sur Drive/GitHub.
-ADDIN_VERSION = '1.78'
+ADDIN_VERSION = '1.79'
 
 app = None
 ui = None
